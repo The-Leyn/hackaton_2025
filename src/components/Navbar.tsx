a91ea796f7295e0b2fe4514ec6c357f72c082045
Adrien Allard <allard15@hotmail.fr>
2025-03-18 16:11:28 +0100

    Add NavBar Component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,27 @@
+'use client'
 import React from "react";
 import Link from "next/link";
+import { House, UserRound, Medal } from "lucide-react";
+import { usePathname } from "next/navigation";
 export default function Navbar() {
+  const pathname = usePathname();
   return (
-    <nav className="flex fixed bottom-0 bg-purple-400 w-full p-8 items-center justify-center gap-4">
-      <button>
-        <Link href={`/profile`}>
-        
+    <nav className="flex fixed bottom-0 bg-hackatonWhite-400 rounded-t-2xl w-full p-4 items-center justify-around">
+      <button className={`p-2 rounded-xl ${pathname === "/" ? "bg-hackatonPervenche-400" : ""}`}>
+        <Link href={`/`}>
+          <House className="text-hackatonPervencheDark-400" size={32} />
         </Link>
       </button>
-      <Link href={`/profile`}>Profile</Link>
-      <Link href={`/profile`}>Profile</Link>
-      <Link href={`/profile`}>Profile</Link>
+      <button className={`p-2 rounded-xl ${pathname === "/ranking" ? "bg-hackatonPervenche-400" : ""}`}>
+        <Link href={`/ranking`}>
+          <Medal className="text-hackatonPervencheDark-400" size={32} />
+        </Link>
+      </button>
+      <button className={`p-2 rounded-xl ${pathname === "/profile" ? "bg-hackatonPervenche-400" : ""}`}>
+        <Link href={`/profile`}>
+          <UserRound className="text-hackatonPervencheDark-400" size={32} />
+        </Link>
+      </button>
     </nav>
   );
 }
