6bea6ba7465591d7dac652a086e0050f550a1e1b
Woodis <stephane78.descarpentries@gmail.com>
2025-03-20 12:59:03 +0100

    profil page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,28 @@
 import Image from "next/image";
 import { auth, currentUser } from "@clerk/nextjs/server";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, UserButton, UserProfile, SignOutButton } from "@clerk/nextjs";
+import "@/app/globals.css";
+
 export default async function Home() {
   return (
     <div className="">
       <main className="h-screen w-screen bg-slate-50">
         <SignedIn>
-          <UserButton />
+          <UserProfile 
+            appearance={{
+              elements: {
+                rootBox: "w-full!",
+                cardBox: "w-screen! max-w-none!",
+              },
+            }}
+          />
+          <div className="flex justify-center items-center mt-3">
+            <SignOutButton>
+              <button className="backgrounPervenche text-white font-bold py-2 px-4 rounded">
+                Se déconnecter
+              </button>
+            </SignOutButton>
+          </div>
         </SignedIn>
       </main>
     </div>
